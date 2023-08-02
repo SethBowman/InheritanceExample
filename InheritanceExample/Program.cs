8e86a9a8ac2c209b0d7439f87dced0b399407356
Seth Bowman <brandonsethbowman@gmail.com>
2023-08-02 10:01:46 -0500

    Added list of type person with foreach loop

--- InheritanceExample/Program.cs
+++ InheritanceExample/Program.cs
@@ -7,9 +7,11 @@ var seth = new Coder();
 seth.Name = "Seth";
 
 seth.ProgrammingLanguage = "C#";
-seth.Code();
 
 seth.Greet();
+seth.Code();
+
+Console.WriteLine();
 
 var bob = new Person();
 
@@ -17,6 +19,7 @@ bob.Name = "Bob";
 
 bob.Greet();
 
+Console.WriteLine();
 
 var bill = new Athlete();
 
@@ -28,3 +31,27 @@ bill.Sport = "Football";
 bill.PlayGame();
 
 bill.Code();
+Console.WriteLine();
+
+var people = new List<Person>() 
+{ 
+    seth, 
+    bob, 
+    bill
+};
+
+foreach(var person in people)
+{
+    if (person is Coder)
+    {
+        Console.WriteLine($"My name is {person.Name} and I am a coder!");
+    }
+    else if (person is Athlete)
+    {
+        Console.WriteLine($"My name is {person.Name} and I play sports!");
+    }
+    else
+    {
+        Console.WriteLine($"My name is {person.Name} and I'm jus a regular ole guy.");
+    }
+}
